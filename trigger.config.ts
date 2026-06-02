e751394f087cf3b29617a696446a076628d2b61a
Nabil Jint <yusufjintoro@gmail.com>
2026-06-02 09:27:25 +0100

    push to main

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "@trigger.dev/sdk";
-import { prismaExtension } from "@trigger.dev/build/extensions/prisma";
 
 export default defineConfig({
   project: process.env.TRIGGER_PROJECT_REF!,
@@ -14,13 +13,5 @@ export default defineConfig({
       maxTimeoutInMs: 10000,
       factor: 2,
     },
-  },
-  build: {
-    extensions: [
-      prismaExtension({
-        schema: "prisma/schema.prisma",
-        migrate: false,
-      }),
-    ],
   },
 });
